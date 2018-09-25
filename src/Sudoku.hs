bf4503277edef22398c6a03bd0fd17abbd721695
wasabi315 <wasabi.nvim.st@gmail.com>
2018-09-25 11:21:10 +0900

    refactor

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -13,11 +13,11 @@ module Sudoku where
 import           Data.Char             ( digitToInt, intToDigit )
 import           Data.IntMap.Strict    ( IntMap, (!) )
 import qualified Data.IntMap.Strict    as IM
-import           Data.List             ( sort, delete )
+import           Data.List             ( sort, delete, minimumBy )
 import           Data.Monoid           ( All(..), Any(..) )
+import           Data.Ord              ( comparing )
 
 -- Types ----------------------------------------------------------------------
-
 
 data Pos = Pos
     { row :: Int
@@ -25,9 +25,7 @@ data Pos = Pos
     , sqr :: Int
     } deriving (Eq, Show, Ord)
 
-
 type Board = IntMap [Pos]
-
 
 toBoard :: String -> Maybe Board
 toBoard s
@@ -40,7 +38,6 @@ toBoard s
     f (p, n) = IM.adjust (p:) (readVal n)
     readVal '.' = 0
     readVal n   = digitToInt n
-
 
 showBoard :: Board -> String
 showBoard = map intToDigit . toList
@@ -60,62 +57,41 @@ Pos r1 c1 s1 #! Pos r2 c2 s2
     && c1 /= c2
     && s1 /= s2
 
-
 assign :: Int -> Pos -> Board -> Board
 assign n p = IM.adjust (p:) n . IM.adjust (delete p) 0
-
 
 notMemberOn :: (Pos -> Int) -> Pos -> [Pos] -> Bool
 notMemberOn f p ps = f p `notElem` map f ps
 
-
 sieve :: Pos -> [Pos] -> Bool
 sieve p = getAll . foldMap (All . (#! p))
-
 
 independent :: Pos -> [Pos] -> Bool
 independent p ps = getAny $ foldMap (\f -> Any $ notMemberOn f p ps) [row, col, sqr]
 
-
 independents :: [Pos] -> [Pos]
 independents ps = filter (\p -> independent p (delete p ps)) ps
-
 
 determineBy :: Int -> Board -> Board
 determineBy n b =
     foldr (assign n) b $ independents $ filter (`sieve` (b ! n)) (b ! 0)
 
-
 determine :: Board -> Board
 determine b = foldr determineBy b [1..9]
-
 
 determineAll :: Board -> Board
 determineAll b =
     let b' = determine b
     in  if b == b' then b else determineAll b'
 
-
 candidatesAt :: Pos -> Board -> [Int]
 candidatesAt p b = [ n | n <- [1..9], sieve p (b ! n) ]
 
-
-candidates :: Board -> [[Int]]
-candidates b = map (`candidatesAt` b) (b ! 0)
-
-
-minIndex :: [Int] -> Int
-minIndex = snd . minimum . (`zip` [0..])
-
-
 assumptions :: Board -> [Board]
 assumptions b =
-    let css  = candidates b
-        i    = minIndex $ map length css
-        cand = css !! i
-        co   = (b ! 0) !! i
-    in [ assign n co b | n <- cand ]
-
+    let ps      = map (\p -> (p, candidatesAt p b)) (b ! 0)
+        (p, cs) = minimumBy (comparing (length . snd)) ps
+    in  map (\c -> assign c p b) cs
 
 solver :: Board -> [Board]
 solver b
@@ -125,10 +101,8 @@ solver b
     where
         b' = determineAll b
 
-
 wrong :: Board -> Bool
 wrong b = getAny $ foldMap (Any . dup) (IM.delete 0 b)
-
 
 dup :: [Pos] -> Bool
 dup ps = getAny $ foldMap (Any . not . (\p -> p `sieve` delete p ps)) ps
