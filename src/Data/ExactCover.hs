4deb71177edf7a9e233a81a383596c1666850ea9
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-07-11 22:22:04 +0900

    Add Document

--- src/Data/ExactCover.hs
+++ src/Data/ExactCover.hs
@@ -30,6 +30,19 @@ type Matrix = IM.IntMap Column
 -------------------------------------------------------------------------------
 -- ALGORITHM
 
+-- Knuth's Algorithm X (https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X)
+-- 1. If the matrix A has no columns, the current partial solution is
+--    a valid solution; terminate successfully.
+-- 2. Otherwise choose a column c (deterministically).
+-- 3. Choose a row r such that Ar, c = 1 (nondeterministically).
+-- 4. Include row r in the partial solution.
+-- 5. For each column j such that Ar, j = 1,
+--        for each row i such that Ai, j = 1,
+--            delete row i from matrix A.
+--        delete column j from matrix A.
+-- 6. Repeat this algorithm recursively on the reduced matrix A.
+
+-- STEP 2
 minSizeCol :: Matrix -> Maybe Column
 minSizeCol m
     | IS.null c = Nothing
@@ -38,6 +51,7 @@ minSizeCol m
     c = minimumBy (comparing IS.size) m
 
 
+-- STEP 5
 delete :: Matrix -> Int -> Matrix
 delete m r = IM.map (`IS.difference` s) f
   where
