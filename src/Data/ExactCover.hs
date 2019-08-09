0daa483d4a6d77885ce587ee307f16172ae84f95
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-08-10 06:48:05 +0900

    Speed up

--- src/Data/ExactCover.hs
+++ src/Data/ExactCover.hs
@@ -14,12 +14,12 @@
 module Data.ExactCover where
 
 import           Control.Applicative
-import           Control.Monad
 import           Data.Foldable
-import           Data.Function
 import qualified Data.IntMap as IM
 import qualified Data.IntSet as IS
 import           Data.Ord
+
+import           Data.Foldable.Extra
 
 -------------------------------------------------------------------------------
 -- TYPES
@@ -30,30 +30,13 @@ type Matrix = IM.IntMap Column
 -------------------------------------------------------------------------------
 -- ALGORITHM
 
--- Knuth's Algorithm X (https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X)
--- 1. If the matrix A has no columns, the current partial solution is
---    a valid solution; terminate successfully.
--- 2. Otherwise choose a column c (deterministically).
--- 3. Choose a row r such that Ar, c = 1 (nondeterministically).
--- 4. Include row r in the partial solution.
--- 5. For each column j such that Ar, j = 1,
---        for each row i such that Ai, j = 1,
---            delete row i from matrix A.
---        delete column j from matrix A.
--- 6. Repeat this algorithm recursively on the reduced matrix A.
-
--- STEP 2
 minSizeCol :: Matrix -> Maybe Column
-minSizeCol m
-    | IS.null c = Nothing
-    | otherwise = pure c
-  where
-    c = minimumBy (comparing IS.size) m
+minSizeCol (IM.null -> True) = Nothing
+minSizeCol m = Just $ minimumBy (comparing IS.size) m
 
 
--- STEP 5
-delete :: Matrix -> Int -> Matrix
-delete m r = IM.map (`IS.difference` s) f
+shrink :: Matrix -> Int -> Matrix
+shrink m r = IM.map (`IS.difference` s) f
   where
     (t, f) = IM.partition (IS.member r) m
     s      = IS.unions t
@@ -65,15 +48,13 @@ algX m = case minSizeCol m of
     Nothing -> []
     Just rs -> do
         r <- IS.toAscList rs
-        IS.insert r <$> algX (delete m r)
+        IS.insert r <$> algX (shrink m r)
 
 -------------------------------------------------------------------------------
 
 toMatrix :: [[Int]] -> Matrix
-toMatrix = foldr phi (const IM.empty) [(0 :: Int) ..]
+toMatrix = ifoldr phi IM.empty
   where
-    phi _ _ []       = IM.empty
-    phi r k (cs:css) = foldr psi (k css) cs
-      where
-        psi = IM.alter (Just . maybe (IS.singleton r) (IS.insert r))
+    phi r = flip . foldr . IM.alter $ \m ->
+        IS.insert r <$> m <|> pure (IS.singleton r)
 
--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns #-}
+
 -------------------------------------------------------------------------------
 -- |
 -- Module      : Sudoku
@@ -11,15 +13,17 @@
 
 module Sudoku where
 
-import           Control.Arrow
 import           Data.Char
-import           Data.Foldable
-import           Data.Function
 import qualified Data.IntSet as IS
-import           Data.List   ( unfoldr )
-import           Data.Maybe
 
 import           Data.ExactCover
+import           Data.Foldable.Extra
+
+-------------------------------------------------------------------------------
+-- UTIL
+
+force :: a -> a
+force x = x `seq` x
 
 -------------------------------------------------------------------------------
 -- CONSTANT
@@ -27,11 +31,12 @@ import           Data.ExactCover
 sudokuConstraint :: Matrix
 sudokuConstraint = toMatrix $ map cstr [0 .. 728]
   where
+    cstr :: Int -> [Int]
     cstr x =
-        [ i
-        , 9 * r + n + 81
-        , 9 * c + n + 162
-        , 9 * b + n + 243
+        [ force i
+        , force $ 9 * r + n + 81
+        , force $ 9 * c + n + 162
+        , force $ 9 * b + n + 243
         ]
       where
         (i, n) = x `divMod` 9
@@ -41,36 +46,26 @@ sudokuConstraint = toMatrix $ map cstr [0 .. 728]
 -------------------------------------------------------------------------------
 -- PROCESSING
 
-readCell :: Char -> Maybe Int
-readCell c =
-    if isDigit c && c > '0'
-        then Just $! digitToInt c - 1
-        else Nothing
-
-
 readSudoku :: String -> Maybe IS.IntSet
-readSudoku str
-    | length str /= 81 = Nothing
-    | otherwise
-        = zip [(0 :: Int) ..] str
-        & mapMaybe (\(i, c) -> (+ (9 * i)) <$> readCell c)
-        & IS.fromAscList
-        & Just
+readSudoku str@(length -> 81) = Just . IS.fromAscList $ ifoldr phi [] str
+  where
+    phi i c xs = if isDigit c && c > '0'
+        then (9 * i + digitToInt c - 1) : xs
+        else xs
+readSudoku _ = Nothing
 
 
 showSudoku :: IS.IntSet -> String
-showSudoku
-    =   IS.toAscList
-    >>> map (\i -> intToDigit $ i `mod` 9 + 1)
+showSudoku = map (\i -> intToDigit $! i `mod` 9 + 1) . IS.toAscList
 
 -------------------------------------------------------------------------------
 -- MAIN
 
 solveSudoku :: String -> [String]
-solveSudoku = readSudoku >>> maybe [] solve
+solveSudoku = maybe [] solve . readSudoku
   where
     solve is
-        = IS.foldl' delete sudokuConstraint is
-        & algX
-        & map (showSudoku . IS.union is)
+        = map (showSudoku . IS.union is)
+        . algX
+        $ IS.foldl' shrink sudokuConstraint is
 
