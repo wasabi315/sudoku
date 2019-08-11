c61dd4aa71a77ed100f18856e4b7084241460b19
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-08-11 11:22:23 +0900

    Add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import           Data.List.Split
+import           Test.HUnit
+
+import           Sudoku
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    ps <- lines <$> readFile "./test/sudoku.csv"
+    let ts = map genTest ps
+    runTestTT (TestList ts)
+    pure ()
+
+
+genTest :: String -> Test
+genTest t = "test" ~: solveSudoku p ~?= [s]
+  where
+    [p, s] = wordsBy (== ',') t
+
