afbcf540d2f5b91e07b25c28db6a96e08b6e0c21
wasabi315 <wasabi.nvim.st@gmail.com>
2021-02-22 14:47:53 +0900

    Fix

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,15 @@ module Main where
 import Control.Arrow
 import Control.Monad
 import Data.Foldable
+import Data.Maybe
 import Sudoku qualified
 
 main :: IO ()
 main =
   do
     ps <- lines <$> getContents
-    for_ ps $ traverse_ putStrLn <<< Sudoku.solve <=< Sudoku.parse
+    for_ ps $
+      putStrLn
+        <<< fromMaybe "No solution found"
+        <<< Sudoku.solve
+        <=< Sudoku.parse
